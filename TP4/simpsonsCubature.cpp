b9f2ffe6b2f81da9e23cdb6bf629de95a9d16b6e
Mário Gil <mg_mesquita@hotmail.com>
2018-10-16 12:16:04 +0100

    Implemented cubature methods

--- TP4/simpsonsCubature.cpp
+++ TP4/simpsonsCubature.cpp
@@ -10,7 +10,7 @@ double trapeziumCubature(int n, double x0, double x1, double y0, double y1)
 	double hx = (x1 - x0) / n;
 	double hy = (y1 - y0) / n;
 
-	double result = hx * hy / 4;
+	double result = hx * hy / 9;
 
 	double sum0 = 0, sum1 = 0, sum2 = 0;
 
@@ -18,7 +18,7 @@ double trapeziumCubature(int n, double x0, double x1, double y0, double y1)
 	sum0 = f(x0, y0) + f(x0, y1) + f(x1, y0) + f(x1, y1);
 
 	// SUM 1
-	for (double x = x0+hx; x <= x1-hx; x += hx)
+	for (double x = x0 + hx; x <= x1 - hx; x += hx)
 	{
 		sum1 += (f(x, y0) + f(x, y1));
 	}
@@ -32,7 +32,7 @@ double trapeziumCubature(int n, double x0, double x1, double y0, double y1)
 		for (double y = y0 + hy; y <= y1 - hy; y += hy)
 			sum2 += f(x, y);
 
-	result *= (sum0 + 2 * sum1 + 4 * sum2);
+	result *= (sum0 + 4 * sum1 + 16 * sum2);
 	return result;
 }
 
